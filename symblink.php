4ed91f2456a28729b4d24c39da3eac1670f8eeba
rahmanbayu <rahmanbayu4@gmail.com>
2024-02-26 09:14:48 +0700

    u

--- symblink.php
+++ symblink.php
@@ -1,9 +1,12 @@
 <?php
-$target = '/home/dkppseruyango/repositories/alsitan.dkpp.seruyankab.go.id/storage/app/public';
-$shortcut = '/home/dkppseruyango/public_html/storage';
+$target = '/home/dkppseruyango/repositories/alsitan/storage/app/public';
+$shortcut = '/home/dkppseruyango/public_html/alsitan.dkpp.seruyankab.go.id/storage';
 symlink($target, $shortcut);
 
-$target = '/home/dkppseruyango/repositories/alsitan.dkpp.seruyankab.go.id/public/build';
-$shortcut = '/home/dkppseruyango/public_html/';
+$target = '/home/dkppseruyango/repositories/alsitan/public/build';
+$shortcut = '/home/dkppseruyango/public_html/alsitan.dkpp.seruyankab.go.id';
 symlink($target, $shortcut);
+
+echo 'oke';
+// ln -s /home/dkppseruyango/repositories/alsitan/storage/app/public /home/dkppseruyango/public_html/storage
 ?>
